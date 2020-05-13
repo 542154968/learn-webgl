dfb78b45cdf9583944f86ddce655b83816995935
542154968 <542154968@qq.com>
2020-05-13 11:46:13 +0800

    fix: menu路由首次进入不会自动切换的问题

--- src/layout/Aside/index.vue
+++ src/layout/Aside/index.vue
@@ -1,11 +1,5 @@
 <template>
-  <el-menu
-    :default-active="routerPath"
-    class="el-menu-vertical-demo"
-    @open="handleOpen"
-    @close="handleClose"
-    router
-  >
+  <el-menu :default-active="routerPath" @open="handleOpen" @close="handleClose" router>
     <template v-for="item in routes">
       <el-submenu
         :key="item.path"
@@ -28,7 +22,7 @@
 </template>
 
 <script lang="ts">
-import { createComponent } from "@vue/composition-api";
+import { createComponent, ref, onMounted, watch } from "@vue/composition-api";
 import routes from "./routes";
 import MenuItem from "./MenuItem";
 import MenuContain from "./MenuContain";
@@ -38,13 +32,34 @@ export default createComponent({
   name: "aside-menu",
   components: { MenuItem, MenuContain },
   setup(prop, { root }) {
+    const routerPath = ref("");
+
+    let watchNum = 1;
+
+    const watchId = watch(
+      () => root.$route.path,
+      next => {
+        routerPath.value = next;
+        watchNum++;
+        // 刷新的时候设置高亮路由 需要设置 但是路由更新 ref路由并不会响应更新
+        // 所以就通过watch来手动触发更新
+        // 更新之后默认高亮就正确了
+        // 随着点击 高亮会自动切换
+        // 所以就不需要继续监听了
+        // 所以在这里终止监听
+        watchNum > 2 && watchId();
+      }
+    );
+
     function handleOpen() {}
+
     function handleClose() {}
+
     return {
       handleOpen,
       handleClose,
       routes,
-      routerPath: root.$route.path
+      routerPath
     };
   }
 });
