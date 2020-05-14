85003bb64eeca231b329d79cb747550a2b0d28fd
542154968 <542154968@qq.com>
2020-05-14 13:32:44 +0800

    feat: 第一个webgl文件

--- src/views/canvas/index.tsx
+++ src/views/canvas/index.tsx
@@ -1,8 +1,28 @@
-import { createComponent } from "@vue/composition-api";
+import { createComponent, onMounted } from "@vue/composition-api";
 import NiceContain from "@components/NiceContain";
+import { getWebGLContext } from "@lib/cuon-utils";
 
 export default createComponent({
-  setup() {
-    return () => <NiceContain>let's begin!</NiceContain>;
+  setup(props, { refs }) {
+    onMounted(() => {
+      initCanvas();
+    });
+
+    function initCanvas() {
+      const canvas = refs["el-webgl"];
+      let gl = getWebGLContext(canvas);
+      console.log(gl);
+      if (!gl) {
+        console.log("Faile to load");
+      }
+      gl!.clearColor(1, 0.5, 0.5, 3);
+      gl!.clear(gl!.COLOR_BUFFER_BIT);
+    }
+
+    return () => (
+      <NiceContain>
+        <canvas id="wegbl" ref="el-webgl" width="400" height="400"></canvas>
+      </NiceContain>
+    );
   }
 });
