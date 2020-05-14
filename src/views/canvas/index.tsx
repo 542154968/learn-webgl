ca42a049bc3583fef075d45613d3cfd9d6b2a3dd
542154968 <542154968@qq.com>
2020-05-14 15:24:05 +0800

    feat(最短的webgl): 样式优化

--- src/views/canvas/index.tsx
+++ src/views/canvas/index.tsx
@@ -29,9 +29,6 @@ export default createComponent({
           if (!gl) {
             console.log("Faile to load");
           }
-          break 
-          new 
-          const age = 18;
           gl!.clearColor(1, 0.5, 0.5, 3);
           gl!.clear(gl!.COLOR_BUFFER_BIT);
           `}
