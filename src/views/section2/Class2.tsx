49ded36edf2268474132d2e929d1f91cb288205f
542154968 <542154968@qq.com>
2020-05-15 17:29:59 +0800

    feat: 代码块位置调整

--- src/views/section2/Class2.tsx
+++ src/views/section2/Class2.tsx
@@ -1,35 +1,55 @@
-import { createComponent, onMounted } from "@vue/composition-api";
+import { createComponent } from "@vue/composition-api";
 import CodeView from "@components/CodeView";
 import NiceContain from "@components/NiceContain";
 import WebGLContain from "@/components/WebGLContain";
+import { initShaders } from "@lib/cuon-utils";
+import { codeStr as class1CodeStr } from "./Class1";
 
+// 顶点着色器
+const VSHADER_SOURCE = `
+ void main(){
+   // 设置坐标
+   gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+   // 设置尺寸
+   gl_PointSize = 10.0;
+ }
+`;
+
+// 片元着色器
+const FSHADER_SOURCE = `
+ void main(){
+   // 设置颜色
+   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+ }
+`;
+
+function handleCanvasReady(gl: WebGLRenderingContext) {
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log("failed to initialize shaders");
+    return;
+  }
+  // 设置canvas背景色
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // 清空canvas
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  // 绘制一个点
+  gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+const codeStr = `
+// 顶点着色器
+const VSHADER_SOURCE = \`${VSHADER_SOURCE}\`;
+// 片元着色器
+const FSHADER_SOURCE = \`${FSHADER_SOURCE}\`;
+${class1CodeStr}
+gl.drawArrays(gl.POINTS, 0, 1);
+`;
 export default createComponent({
-  setup(props, { root }) {
-    function handleCanvasReady(gl: WebGLRenderingContext) {
-      console.log(gl);
-    }
-
+  setup() {
     return () => (
       <NiceContain>
         <WebGLContain onCanvasReady={handleCanvasReady}></WebGLContain>
-        <CodeView
-          code={`
-          const canvas = refs["el-webgl"];
-          let gl = getWebGLContext(canvas);
-          if (!gl) {
-            console.log("Faile to load");
-          }
-          
-          // rgba
-          // 背景色在下一次调用gl.clearColor之前不会变
-          gl!.clearColor(1, 0.5, 0.5, 3);
-
-          // 清空绘图区域实际上是清除颜色缓冲区 所以传入的是个COLOR_BUFFER_BIT
-          // gl.clear继承与OpenGL
-          gl!.clear(gl!.COLOR_BUFFER_BIT);
-
-          `}
-        ></CodeView>
+        <CodeView code={codeStr}></CodeView>
       </NiceContain>
     );
   }
--- src/views/section2/class1.tsx
+++ src/views/section2/class1.tsx
@@ -3,35 +3,34 @@ import CodeView from "@components/CodeView";
 import WebGLContain from "@/components/WebGLContain";
 import NiceContain from "@components/NiceContain";
 
+function handleCanvasReady(gl: WebGLRenderingContext) {
+  gl.clearColor(1, 0.5, 0.5, 3);
+  gl.clear(gl!.COLOR_BUFFER_BIT);
+}
+
+const codeStr = `
+const canvas = refs["el-webgl"];
+let gl = getWebGLContext(canvas);
+if (!gl) {
+  console.log("Faile to load");
+}
+// rgba
+// 背景色在下一次调用gl.clearColor之前不会变
+gl!.clearColor(1, 0.5, 0.5, 3);
+// 清空绘图区域实际上是清除颜色缓冲区 所以传入的是个COLOR_BUFFER_BIT
+// gl.clear继承与OpenGL
+gl!.clear(gl!.COLOR_BUFFER_BIT);
+`;
+
 export default createComponent({
   setup() {
-    function handleCanvasReady(gl: WebGLRenderingContext) {
-      gl.clearColor(1, 0.5, 0.5, 3);
-      gl.clear(gl!.COLOR_BUFFER_BIT);
-    }
-
     return () => (
       <NiceContain>
         <WebGLContain onCanvasReady={handleCanvasReady}></WebGLContain>
-        <CodeView
-          code={`
-          const canvas = refs["el-webgl"];
-          let gl = getWebGLContext(canvas);
-          if (!gl) {
-            console.log("Faile to load");
-          }
-          
-          // rgba
-          // 背景色在下一次调用gl.clearColor之前不会变
-          gl!.clearColor(1, 0.5, 0.5, 3);
-
-          // 清空绘图区域实际上是清除颜色缓冲区 所以传入的是个COLOR_BUFFER_BIT
-          // gl.clear继承与OpenGL
-          gl!.clear(gl!.COLOR_BUFFER_BIT);
-
-          `}
-        ></CodeView>
+        <CodeView code={codeStr}></CodeView>
       </NiceContain>
     );
   }
 });
+
+export { codeStr };
