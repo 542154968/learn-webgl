58d66e7904cf0d89638ab06a143a1ba8b3dde456
542154968 <542154968@qq.com>
2020-05-15 14:33:40 +0800

    feat: WebGLContain 组件封装

---
+++ src/components/WebGLContain.tsx
@@ -0,0 +1,52 @@
+import { createComponent, onMounted } from "@vue/composition-api";
+import NiceContain from "@components/NiceContain";
+import { getWebGLContext } from "@lib/cuon-utils";
+
+export default createComponent({
+  props: {
+    onCanvasReady: {
+      type: Function,
+      require: true
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    }
+  },
+  setup(
+    props: {
+      onCanvasReady: (gl: WebGLRenderingContext | null) => {};
+      width?: number;
+      height?: number;
+    },
+    { refs, emit }
+  ) {
+    onMounted(() => {
+      initCanvas();
+    });
+
+    function initCanvas() {
+      const canvas = refs["el-webgl"];
+      let gl = getWebGLContext(canvas);
+      if (!gl) {
+        console.log("Faile to load");
+      }
+      emit("canvasReady", gl);
+    }
+
+    return () => (
+      <NiceContain>
+        <canvas
+          id="wegbl"
+          ref="el-webgl"
+          width={props.width}
+          height={props.height}
+        ></canvas>
+      </NiceContain>
+    );
+  }
+});
